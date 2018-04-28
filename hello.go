8d6bf370ed34a8c91558e62434fe52cb03708bf1
Mochamad Rizal Jauhari <jauhari@ainosi.co.id>
2018-04-29 00:08:45 +0700

    update everything

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"./pkg-lib"
+)
+
+func main() {
+	fmt.Println("Hello World\n")
+	libstring.ThisFunc("hello there")
+}
---
+++ pkg-lib/lib.go
@@ -0,0 +1,7 @@
+package libstring
+
+import "fmt"
+
+func ThisFunc(message string) {
+	fmt.Println(message)
+}
